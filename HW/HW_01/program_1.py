8a77504d34761c8357db15c8cc8fac7a4cc4a98e
Misha994 <m.petrytsyn@gmail.com>
2017-06-22 00:42:36 +0300

    HW1 program1,2

---
+++ HW/HW_01/program_1.py
@@ -0,0 +1,6 @@
+k = str(input('k = '))
+
+if len(k) < 3:
+    print 'Your number must contain more than three characters'
+else:
+    print 'h = ', k[2]
---
+++ HW/HW_01/program_2.py
@@ -0,0 +1,5 @@
+x = float(input('x = '))
+
+print 'x = ' , x
+x = '%.1f' % x
+print 'b = ' , x[-1]
