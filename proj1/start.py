f1bf950cfa6af9a6bb2cd7132a09be133b3218f1
poofpwnz <karvachkiy@gmail.com>
2017-06-22 14:42:14 +0300

    add HomeWork

---
+++ proj1/start.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#Exercise 1
+#a = int(1234567)
+#print 'a = ', a
+#b = int(a%1000)//100
+#print 'b = ', b
+#Exercise 2!
+#a = float(32.597)
+#print 'a =', a
+#b = int(a*10)%10
+#print 'b= ', b
+#Exercise 2!
+#x = float(32.597)
+#d = int(x)
+#if d == 0:
+#    y = x * 10
+ #   z = int(y)
+ #   print (z)
+#if d > 0:
+#    print (int(10 * (x - int(x))))
+#Exercise 3
+#x = int(123)
+#print ((x // 100) + ((x // 10) % 10) + (x - (x // 10) * 10))
+
