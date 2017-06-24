ee717ba38c6a768391480aec53898a2b8f4a072e
Olexandr Zhytenko <zhytenko@ukr.net>
2017-06-24 23:26:32 +0300

    first commit

---
+++ lesson01/main.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+
+# s = 'text'
+# print(type(s))
+#
+# s1 = u'text'
+# print(type(s1))
+#
+# print(type('text') == type('text'))
+
+#s = '0123456789'
+#
+# print(s[::-1])
+#print(s)
+
+#print(dir(s))
+#print(s.count('46-1a'))
+#print(help(str.format))
+# print('name %s age %s' % ('Roma', 25))
+# print('name {1} age {2}{0}'.format(3, 'Roma', 25))
+
+# i = 10
+# print(i)
+#
+# i = 0x10
+# print(i)
+#
+# i = len(str(int(2**31)**1000))
+# print(i)
+
+#
+# i = float(0.5)
+# print(i)
+#
+# i = -1.54E-21
+# print(i)
+#
+# i = 5 + 5.0
+# print(i)
+# a, b = 8, 2
+# print(a//b)
+# print(a%b)
+
+# print((-9)**(-2.2))
+
+# print(True and not False)
+# print(not 0)
