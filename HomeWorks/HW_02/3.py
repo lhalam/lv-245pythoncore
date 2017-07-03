0c4ea060e82243c9bdbd976f558a013fd08381cd
Kyrylo Kovalchuk <kyrylo.dubai@gmail.com>
2017-07-03 18:28:25 +0300

    add HW

---
+++ HomeWorks/HW_02/3.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+s = "Perestanovka"
+
+x = "Perestanovka"
+y = "akvonatsereP"
+
+
+def fun(x,y):
+    a = 0
+    for i in range(len(x)):
+        for j in range(len(x)):
+            if x[j] is y[i]:
+                a += 1
+                if a == len(x):
+                    return x
+
+print fun(x,y)
