d94b4f21c9bfdbd154446a942f08439eec91ca20
Olexandr Zhytenko <zhytenko@ukr.net>
2017-07-03 14:17:34 +0300

    add second task
    Signed-off-by: Olexandr Zhytenko <zhytenko@ukr.net>

--- homeworks/HW_02.py
+++ homeworks/HW_02.py
@@ -39,8 +39,6 @@ def fraction_calculation(a, b, c, x, y, z):
                numerators_calculation(b, y))
               / denominator_calculation(c, x, z))
     return round(result, 3)
-
-
 # show result
 a = create_variables('a')
 b = create_variables('b')
@@ -48,7 +46,6 @@ c = create_variables('c')
 x, y, z = 2, 2, 2
 print('result of exercise 1 is:',
       fraction_calculation(a, b, c, x, y, z))
-
 
 # exercise 2
 def calculate_factorial(number):
@@ -60,23 +57,20 @@ def calculate_factorial(number):
             result *= i
     return result
 
-
 # to calculate factorial - opinion 2
 # def calculate_factorial(numbers):
 #     from functools import reduce
 #     if numbers == 0:
 #         return 1
 #     else:
-#         return (reduce(lambda number, result=1: number * result,
-#                    [i for i in range(1, numbers + 1)]))
+#         return reduce(lambda number, result=1: number * result,\
+#                    [i for i in range(1, numbers + 1)])
 
 
 def calculate_expression(m, n):
     result = ((calculate_factorial(n) * calculate_factorial(m)) /
               calculate_factorial(n + m))
     return result
-
-
 # show result
 print('result of exercise 2 is:',
       round(calculate_expression(2, 2), 3))
