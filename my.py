b20a53be49324a689f366f5312c9e6452bc9059b
Sereda Ihor <seredaihor@ukr.net>
2017-07-03 17:56:38 +0300

    Fibanachi

--- my.py
+++ my.py
@@ -1,58 +1,84 @@
-# # Home work
-# # -----------------------------------------------------------
-# # -----------------------------------------------------------
-# # EX_1
-#
-# # n = int(input("n = "))
+# # # Home work
+# # # -----------------------------------------------------------
+# # # -----------------------------------------------------------
+# # # EX_1
 # #
-# # suma = 0
-# # for i in str(n):
-# #     suma += int(i)
-# # print(suma)
-# #
-# text = "sdkgns df1kngjen gk1jkdfnj gdfg 1df5665dgsf"
-# #
+# # # n = int(input("n = "))
+# # #
+# # # suma = 0
+# # # for i in str(n):
+# # #     suma += int(i)
+# # # print(suma)
+# # #
+# # text = "sdkgns df1kngjen gk1jkdfnj gdfg 1df5665dgsf"
+# # #
+# # # for i in text:
+# # #     if i.isdigit():
+# # #         print(i)
+# # newText = " "
 # # for i in text:
-# #     if i.isdigit():
-# #         print(i)
-# newText = " "
+# #     if i != " ":
+# #         newText += i
+# # #      else:
+# # #         newText[]
+# # help(list())
+#
+# word = []
+# num = []
+#
+# text = "123 124 gjsdk jghd456 465 fjhgs djfhg jksdh fgjksh dgjkk jsdjsa dkfj sd lk"
+# newText = ""
+# L = []
 # for i in text:
-#     if i != " ":
-#         newText += i
-# #      else:
-# #         newText[]
-# help(list())
+#   if  i != " ":
+#       newText += i
+#   else:
+#       L.append(newText)
+#       newText = ""
+#       # for i in L:
+#       #     if i.isdigit():
+#       #         num.append(int(i))
+#       #     else:
+#       #         word.append(str(i))
+# print (L)
+#
+# word = []
+# num = []
+# for i in L:
+#     if i.isdigit():
+#         num.append(int(i))
+#     else:
+#         word.append(str(i))
+#
+# print("Word: " + str(word))
+# print("Numbers: " + str(num))
 
-word = []
-num = []
+#
+# def my_func(b, a):
+#     result = []
+#     for i in range(len(a)):
+#         if a[i]%b == 0:
+#             result.append(a[i])
+#     return result
+#
+# t = my_func(2,[1,1,2,3,4,5,6,7,8, 2, 4])
+# print (t)
 
-text = "123 124 gjsdk jghd456 465 fjhgs djfhg jksdh fgjksh dgjkk jsdjsa dkfj sd lk"
-newText = ""
-L = []
-for i in text:
-  if  i != " ":
-      newText += i
-  else:
-      L.append(newText)
-      newText = ""
-      # for i in L:
-      #     if i.isdigit():
-      #         num.append(int(i))
-      #     else:
-      #         word.append(str(i))
-print (L)
+import math
+def fib(n):
+    List_Fibanachi = []
+    i = 0
+    while True:
+        if i != 0 :
+            SQRT5 = math.sqrt(5)
+            PHI = (SQRT5 + 1) / 2
+            k = int(PHI ** i / SQRT5 + 0.5)
+            if k%2 == 0:
+                List_Fibanachi.append(k)
+        if len(List_Fibanachi) == n:
+            break
+        i += 1
+    return print(List_Fibanachi)
 
-word = []
-num = []
-for i in L:
-    if i.isdigit():
-        num.append(int(i))
-    else:
-        word.append(str(i))
-
-print("Word: " + str(word))
-print("Numbers: " + str(num))
-
-
-
+t = fib(10)
 
