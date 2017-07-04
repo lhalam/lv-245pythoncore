48d8d063367793181600fd260311d19b0b4ca8fe
poofpwnz <karvachkiy@gmail.com>
2017-07-05 00:42:03 +0300

    Add MyHomeWork

---
+++ MyHomeWork/HW1.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#Exercise 1
+#k = int(1234567)
+#print 'k = ', k
+#h = int(k % 1000) // 100
+#print 'h = ', h
+#Exercise 2!
+#x = float(32.597)
+#print 'x =', x
+#d = int(x*10)%10
+#print 'd= ', d
+#Exercise 3
+#x = int(123)
+#print ((x // 100) + ((x // 10) % 10) + (x - (x // 10) * 10))
+#Exercise 4
+#k = 13257
+#h = k // 3600
+#m = (k % 3600) // 60
+#print (h, m)
+#Exercise 7
+#x = int(input())
+#if x % 400 == 0 or x % 4 == 0 and not x % 100 == 0:
+#    print "YES"
+#else:
+#    print "NO"
+#Exercise 8
+#x = 4
+#y = 8
+#x, y = y, x
+#print x, y
+#Exercise 9
+# r = int(input('Задайте радіус - '))
+#i = 2 * r * 3.14  # Довжина кола
+#s = 3.14 * r ** 2  # Площа кола
+#v = 3 / 4 * 3.14 * r ** 3  # Обєм круга
+#print "Довжина кола = {}, Площа кола = {}, Обєм круга = {}".format(i, s, v)
+#Exercise 10
+#k = 9865
+#ch = str(k)
+#s = int(ch[0])+int(ch[1])+int(ch[2])+int(ch[3])
+#print s
+#print type(s)
+#Exercise 16
+#x1 = int(input())
+#y1 = int(input())
+#x2 = int(input())
+#y2 = int(input())
+#if (abs(x1 - x2) == 2 and abs(y1 - y2) == 1) or (abs(x1 - x2) == 1 and abs(y1 - y2) == 2):
+#    print "YES"
+#else:
+#    print "NO"
