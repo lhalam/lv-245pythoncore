7110d22fe420b638fb67181d17c3f6c24c66bb39
Yuriy <yuriy.semesyuk@yahoo.com>
2017-07-04 23:46:03 +0300

    add HW 1 2 3

--- HW/HW_01/HW4.py
+++ HW/HW_01/HW4.py
@@ -1,7 +1,7 @@
-k = int(input())
 
-s = k % 60
-m = ((k - s) // 60) % 60
-h = k // 60 // 60
-
-print (h, m, s)
+k = int(input("vedit kilkist sukynd:    "))
+if k >=0:
+    s = k % 60
+    m = ((k - s) // 60) % 60
+    h = k // 60 // 60
+    print(h, m, s)
