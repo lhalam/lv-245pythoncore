8392da0c870ae97550fa854ede213cb9891b58ff
Sereda Ihor <seredaihor@ukr.net>
2017-07-06 00:30:59 +0300

    Home work №2

--- HomeWork_2.py
+++ HomeWork_2.py
@@ -1,5 +1,9 @@
 # EX_1
 
+# x = int(input("x: "))
+# y = float(input("y: "))
+# z = float(input("z: "))
+
 
 # EX_2
 # F(m,n) = n!*m!/(n+m)!
@@ -19,29 +23,41 @@
 #     print(str("F(m,n) = ") + str(function(m,n)))
 
 # EX_3
-
-# x = str(input("sir pls enter x: "))
-# y = str(input("sir pls enter y: "))
+#
+# import random
 #
 # def Perestanovka(x,y):
+#     for i in range(len(x)):
+#         k = random.choice(x)
+#         y = y + k
+#     return(y)
+#
+# for i in range(4):
+#     x = str(input("sir pls enter word - x: "))
+#     y = ""
+#     y = Perestanovka(x,y)
+#     if x == y:
+#         print(str("word - y: ") + x)
+#     else:
+#         print("слів не утворено за умовою")
 
 # EX_5
-def swap (arr, i, j):
-    arr [i], arr [j] = arr [j], arr [i]
-
-def bubble_sort (arr):
-    i = len (arr)
-    while i> 1:
-        for j in range (i - 1):
-            if arr [j] < arr [j + 1]:
-                swap (arr, j, j + 1)
-        i -= 1
-    return arr
-
-arr_1 = [1, 2, 43, 8, 3, 7]
-arr_2 = [1, 0, 9, 5, 3, 8, 4, 6, 2]
-
-print(bubble_sort(arr_1))
-print(bubble_sort(arr_2))
+# def swap (arr, i, j):
+#     arr [i], arr [j] = arr [j], arr [i]
+#
+# def bubble_sort (arr):
+#     i = len (arr)
+#     while i> 1:
+#         for j in range (i - 1):
+#             if arr [j] < arr [j + 1]:
+#                 swap (arr, j, j + 1)
+#         i -= 1
+#     return arr
+#
+# arr_1 = [1, 2, 43, 8, 3, 7]
+# arr_2 = [1, 0, 9, 5, 3, 8, 4, 6, 2]
+#
+# print(bubble_sort(arr_1))
+# print(bubble_sort(arr_2))
 
 
