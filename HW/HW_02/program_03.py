72b051f27f9932f75dbbe9284df134cfb5d57459
Misha994 <m.petrytsyn@gmail.com>
2017-07-06 17:17:29 +0300

    HW_02

--- HW/HW_02/program_03.py
+++ HW/HW_02/program_03.py
@@ -1,18 +1,11 @@
 x = 'abcd'
 y = 'abcd'
 
-def fun(x,y):
-    a = False
-    if len(x) == len(y):
-        for i in range(len(x)):
-            if x[i] in y:
-                a = True
-            else:
-                a = False
-                break
-    if a:
+def mysort(x,y):
+    if sorted(x) == sorted(y):
         return x
     else:
         return 'False'
 
-print fun(x,y)
+print mysort(x,y)
+
