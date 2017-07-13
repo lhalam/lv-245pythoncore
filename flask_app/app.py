9b284b23c99537b0220500506fc874c51d3bb730
Sereda Ihor <seredaihor@ukr.net>
2017-07-13 21:37:39 +0300

    Flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,11 @@
 import os.path
 
-from flask import Flask, request, redirect,  render_template
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 from form.userForms import UserForm
+
+import hashlib
 
 app = Flask(__name__)
 
@@ -19,8 +21,8 @@ class User(db.Model):
     firstname = db.Column(db.String(20), nullable=False)
     lastname = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(20), nullable=True)
-    
+    password = db.Column(db.String(80), nullable=True)
+
 
 @app.route('/')
 def hello_world():
@@ -28,6 +30,7 @@ def hello_world():
             <a href='http://localhost:5000/user'>user</a><br>
             <a href='http://localhost:5000/user/add'>add user</a><br>
             """
+
 
 @app.route('/test')
 def hello_world_test():
@@ -39,22 +42,22 @@ def user_get():
     users = User.query.all()
     return render_template('user.html', us=users)
 
-@app.route('/user/add', methods=['GET','POST'])
+
+@app.route('/user/add', methods=['GET', 'POST'])
 def user_add():
     form = UserForm(request.form)
     if request.method == 'POST' and form.validate():
-        user = User(username = form.username.data,
-                    firstname = form.firstname.data,
-                    lastname = form.lastname.data,
-                    email = form.email.data,
-                    password = form.password.data)
+        user = User(username=form.username.data,
+                    firstname=form.firstname.data,
+                    lastname=form.lastname.data,
+                    email=form.email.data,
+                    password=hashlib.md5(form.password.data.encode()).hexdigest())
+        print (user.password)
         db.session.add(user)
         db.session.commit()
         return redirect('/user')
+    # print("tesr")
     return render_template('user_add.html', form=form)
-
-
-
 
 
 if __name__ == "__main__":
