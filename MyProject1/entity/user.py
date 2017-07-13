cddab38ff1dfdcdd0140a341e23dc78b0c92b713
poofpwnz <karvachkiy@gmail.com>
2017-07-13 19:37:49 +0300

    add

---
+++ MyProject1/entity/user.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+class User(object):
+    def __init__(self, username, password,  first_name=None, last_name=None, email=None):
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def get_username(self):
+        return self.username
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def get_email(self):
+        return self.email
+
+    def set_password(self, raw_password):
+        self.fr_password = raw_password
+        self.fr = []
+        self.leng = len(self.fr_password)
+        for i in self.fr_password:
+            self.temp = ord(i) +(ord(i) % self.temp)
+            self.code.append(chr(self.temp))
+        return ''.join(self.code)
+
+    def check_password(self, raw_password):
+        self.raw_password = raw_password
+        if  set_password(self.raw_password) == set_password(self.password):
+            return True
+        else:
+            return False
+
+
+    def _is_valid_username(self):
+        if len(self.username) < 50:
+            for i in self.username:
+                if not( i in ("_", "@", "+", ".", "-") or i.isdigit() or i.isalpha()):
+                    return False
+            return True
+        else:
+            return False
+
+    def is_valid(self):
+        errors = []
+        if self._is_valid_username():
+            errors.append("username")
+        return errors
---
+++ lesson10/class_method.py
@@ -0,0 +1,66 @@
+class MyClass(object):
+    name = "name"
+    def __init__(self, arg=None):
+        self.arg = arg
+    def print_self(self):
+        print [i for i in dir(self) if i[0]!="_"]
+    @classmethod       
+    def print_cls(cls):
+        print [i for i in dir(cls) if i[0]!="_"]
+    @staticmethod
+    def static_print(_str=""):
+        print _str
+    @classmethod
+    def class_method(cls):
+        print cls
+    @classmethod
+    def add_new_method(cls, func):
+        cls.new_method = func
+
+
+l = [MyClass(), MyClass(), MyClass()]
+# for i in range(len(l)):
+#     print i,
+#     l[i].print_self()
+#     l[i].print_cls()
+l[0].arg2 = 1
+MyClass.arg3 = 1
+
+# for i in range(len(l)):
+#     print i,
+#     l[i].print_self()
+#     l[i].print_cls()
+
+def my_f(self):
+    print "my_f",
+    print self
+# l[0].f = my_f
+
+# for i in range(len(l)):
+#     print i,
+#     l[i].print_self()
+#     l[i].print_cls()
+# l[0].f(1)
+MyClass.f = my_f
+# for i in range(len(l)):
+#     print i,
+#     l[i].print_self()
+#     l[i].print_cls()
+
+l[0].f()
+
+MyClass.add_new_method(my_f)
+for i in range(len(l)):
+    print i,
+    l[i].print_self()
+    l[i].print_cls()
+
+l[0].new_method()
+
+from ppp import __MyClass
+
+c = __MyClass()
+print c
+print c.x
+print c._x
+print c._MyClass__x
