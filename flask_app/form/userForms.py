a01da41d91ac59b2871553c8478c1febf585c25c
Sereda Ihor <seredaihor@ukr.net>
2017-07-19 17:21:46 +0300

    Flask app

--- flask_app/form/userForms.py
+++ flask_app/form/userForms.py
@@ -2,8 +2,8 @@ from wtforms import Form, StringField, IntegerField, validators
 
 
 class UserForm(Form):
-    username = StringField('User name', [validators.Length(min=2, max=5)])
-    firstname = StringField('First name', [validators.Length(min=2, max=5)])
+    username = StringField('User name', [validators.Length(min=2, max=20)])
+    firstname = StringField('First name', [validators.Length(min=2, max=20)])
     lastname = StringField('Last name', [validators.Length(min=5, max=20)])
     email = StringField('email', [validators.email()])
     password = StringField('password', [validators.Length(min=2, max=20)])
