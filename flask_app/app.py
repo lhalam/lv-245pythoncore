053208e2d2fae79135ce33b58a4b6db9718e7074
Sereda Ihor <seredaihor@ukr.net>
2017-07-21 15:19:12 +0300

    Merge branch 'master' into Mellow88
    # Conflicts:
    #	flask_app/app.py
    #	flask_app/templates/user.html

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,13 +1,9 @@
 import os.path
 import hashlib
 
-from flask import Flask, request, redirect, render_template, send_from_directory
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
-
 from form.userForms import UserForm
-from form.profileForm import ProfileForm
-
-
 
 app = Flask(__name__)
 
@@ -32,96 +28,10 @@ class User(db.Model):
             return user
         except(Exception):
             return None
-    @staticmethod
-    def get_by_id(user_id):
-        try:
-            user = User.query.get(user_id)
-            return user
-        except Exception, e:
-            return None
-
-class Profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    city = db.Column(db.String(20), nullable=True)
-    zip_code = db.Column(db.Integer, nullable=True)
-
-    @staticmethod
-    def get_by_id(user_id):
-        try:
-            profile = Profile.query.get(user_id)
-            return profile
-        except Exception, e:
-            return None
-
-    @staticmethod
-    def get_by_user_id(user_id):
-        try:
-            profile = Profile.query.filter_by(user_id=user_id).first()
-            return profile
-        except Exception, e:
-            return None
-
-class Contact(db.Model):
-    owner_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, primary_key=True)
-
-    @staticmethod
-    def create(owner_id, user_id):
-        contact = Contact(owner_id = owner_id,
-                          user_id = user_id)
-        db.session.add(contact)
-        db.session.commit()
-
-    @staticmethod
-    def get(owner_id):
-        contacts = Contact.query.filter_by(owner_id=owner_id)
-        return contacts
-
-
-
-@app.route('/static/<path:path>')
-def send_js(path):
-    return send_from_directory('static', path)
-
 
 @app.route('/')
 def hello_world():
-    # return """
-    #         <a href='http://localhost:5000/user'>user</a><br>
-    #         <a href='http://localhost:5000/user/add'>add user</a><br>
-    #         """
     return render_template('main.html')
-def index():
-    return """
-            <a href='http://localhost:5000/user'>user</a><br>
-            <a href='http://localhost:5000/user/add'>add user</a><br>
-            """
-
-# http://localhost:5000/user/2/contact?user_id=3
-@app.route('/user/<owner_id>/contact', methods=['GET'])
-def contact(owner_id):
-    add_user_id = request.args.get("user_id")
-    print "add_user_id", add_user_id
-    if add_user_id:
-        add = Contact.query.filter_by(owner_id=owner_id, user_id=add_user_id).all()
-        print "add:", add
-        if not add:
-            Contact.create(owner_id, add_user_id)
-
-    contacts = Contact.get(owner_id=owner_id)
-    users = User.query.all()
-
-    new_users = []
-    for user in users:
-        for contact in contacts:
-            if user.id == contact.user_id:
-                break
-        else:
-            new_users.append(user)
-
-
-    return render_template("contact_list.html", contacts=contacts, users=new_users, owner_id=owner_id)
 
 @app.route('/user', methods=['GET'])
 def user_get():
@@ -141,16 +51,9 @@ def user_add():
         db.session.add(user)
         db.session.commit()
         return redirect('/user')
+    # print("tesr")
     return render_template('user_add.html', form=form)
 
-@app.route('/user/<user_id>/delete', methods=['GET'])
-def user_del(user_id):
-    user = User.get_by_id(user_id)
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-        return redirect('/user')
-    return render_template('error.html', msg_eror="not id {}".format(user_id))
 
 @app.route('/user/<user_id>/delete', methods=['GET'])
 def user_del(user_id):
